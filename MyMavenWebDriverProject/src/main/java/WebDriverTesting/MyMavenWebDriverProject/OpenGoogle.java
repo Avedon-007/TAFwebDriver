ae53ac324369df9ac248c4f395ed813e12103b4a
Avedon-007 <A_N_D_Y-007@mail.ru>
2017-03-13 01:05:28 +0200

    Lesson 6, window of the browser is maximized

--- MyMavenWebDriverProject/src/main/java/WebDriverTesting/MyMavenWebDriverProject/OpenGoogle.java
+++ MyMavenWebDriverProject/src/main/java/WebDriverTesting/MyMavenWebDriverProject/OpenGoogle.java
@@ -10,9 +10,12 @@ public class OpenGoogle
 		System.setProperty("webdriver.gecko.driver", "D:\\Programs\\TEST\\Selenium\\geckodriver.exe");
 		WebDriver myDriver = new FirefoxDriver();
 		
-		myDriver.get("http://google.com");
-		//myDriver.navigate().to("http://google.com");
-
+		//myDriver.get("http://google.com");
+		myDriver.navigate().to("http://google.com");
+		myDriver.manage().window().maximize();
+		
+		myDriver.close();	//close current window (and finish session if it’s the last window)
+		//myDriver.quit();	//close all windows and finish session anyway
 	}
 
 }
