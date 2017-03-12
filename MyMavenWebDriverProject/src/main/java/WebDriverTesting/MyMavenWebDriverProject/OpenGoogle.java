9d71efe232f123d7a044dd375418a9e05d2b4eb0
Avedon-007 <A_N_D_Y-007@mail.ru>
2017-03-13 00:52:16 +0200

    Lesson 6, created launch of Google site

---
+++ MyMavenWebDriverProject/src/main/java/WebDriverTesting/MyMavenWebDriverProject/OpenGoogle.java
@@ -0,0 +1,18 @@
+package WebDriverTesting.MyMavenWebDriverProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class OpenGoogle 
+{
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.gecko.driver", "D:\\Programs\\TEST\\Selenium\\geckodriver.exe");
+		WebDriver myDriver = new FirefoxDriver();
+		
+		myDriver.get("http://google.com");
+		myDriver.navigate().to("http://google.com");
+
+	}
+
+}
