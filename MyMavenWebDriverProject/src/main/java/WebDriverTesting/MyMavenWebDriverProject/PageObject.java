fc583ea4d1760f57736857361438223a979d248c
Avedon-007 <A_N_D_Y-007@mail.ru>
2017-03-13 10:49:49 +0200

    Lesson 6, Create classes of Logging design

---
+++ MyMavenWebDriverProject/src/main/java/WebDriverTesting/MyMavenWebDriverProject/PageObject.java
@@ -0,0 +1,15 @@
+package WebDriverTesting.MyMavenWebDriverProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class PageObject 
+{
+	protected WebDriver driver;
+	
+	public PageObject(WebDriver driver) 
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+}
---
+++ MyMavenWebDriverProject/src/main/java/WebDriverTesting/MyMavenWebDriverProject/StartPage.java
@@ -0,0 +1,5 @@
+package WebDriverTesting.MyMavenWebDriverProject;
+
+public class StartPage {
+
+}
